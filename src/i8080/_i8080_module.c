34eff56cff861d5f025f953db922e14367b7c61f
Chris <chris.ruettimann@gmx.net>
2023-02-05 13:44:28 +0100

    Module method

--- src/i8080/_i8080_module.c
+++ src/i8080/_i8080_module.c
@@ -1,6 +1,36 @@
 #include "Python.h"
 #include "_i8080_module.h"
 
+/* ---------- 
+Module Methods
+----------  */
+static PyObject *
+i8080_m_add(PyObject *self, PyObject *args)
+{
+    uint64_t val_1, val_2;
+
+    // https://docs.python.org/3/c-api/arg.html
+    if (!PyArg_ParseTuple(args, "LL", &val_1, &val_2)){
+        PyErr_SetString(PyExc_Exception, "Parse error\n");
+        return NULL;
+    }
+    uint64_t result = val_1 + val_2;
+    return Py_BuildValue("L", result);
+}
+
+
+/* ---------- 
+Module Initialization
+---------- */
+
+/* List of functions defined in the module */
+
+// https://docs.python.org/3/c-api/structures.html?highlight=pymethoddef#c.PyMethodDef
+static PyMethodDef i8080_module_methods[] = {
+    {"add",    (PyCFunction)i8080_m_add, METH_VARARGS, PyDoc_STR("Add two numbers")},
+    {NULL,              NULL}           /* sentinel */
+};
+
 PyDoc_STRVAR(module_doc,
 "This module is for emulating the Intel 8080 CPU.");
 
@@ -10,7 +40,7 @@ static struct PyModuleDef i8080module = {
     "intel_8080",           //m_name
     module_doc,             //m_doc
     0,                      //m_size
-    NULL,                   //m_methods
+    i8080_module_methods,   //m_methods
     NULL,                   //m_slots
     NULL,                   //m_traverse
     NULL,                   //m_clear
