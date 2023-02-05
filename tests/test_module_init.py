ca70fd1fc223bcdca35fbb948db1edb830d07262
Chris <chris.ruettimann@gmx.net>
2023-02-05 13:51:17 +0100

    add tests

--- tests/test_module_init.py
+++ tests/test_module_init.py
@@ -6,3 +6,7 @@ import _i8080
 def test_docstring():
     assert _i8080.__doc__ == "This module is for emulating the Intel 8080 CPU."
 
+
+def test_add():
+    assert _i8080.add(1, 2) == 3
+    assert _i8080.add(1,-10) == -9
